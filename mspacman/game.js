0c2a5fa2e5da21d56ae2a6007047ec573266226d
CarterCasey <Carter.Casey@tufts.edu>
2015-02-28 14:01:12 -0500

    Wrote the init function, finishing functional piece of lab 5

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,14 @@
-// Replace this with your code...
+function init() {
+	canvas = document.getElementById('game_canvas');
+	ctx = canvas.getContext('2d');
+
+	base_image = new Image();
+	base_image.src = "pacman10-hp-sprite.png";
+
+	base_image.onload = function() {
+		// Load board
+		ctx.drawImage(base_image, 318, 0, 470, 138, 0, 0, 470, 138);
+		// Load Ms Pacman
+		ctx.drawImage(base_image, 80, 20, 20, 20, 150, 60, 20, 20);
+	};
+};
